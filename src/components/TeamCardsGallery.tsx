54dc7efc93cded1897e0042c46cb19a1b1d4d438
Lazaro Alonso <lazarus.alon@gmail.com>
2025-03-05 15:01:12 +0100

    dynamic import

--- src/components/TeamCardsGallery.tsx
+++ src/components/TeamCardsGallery.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import { getMdxFiles, importMdxComponent } from '../constants/loaderMDX';
+import { getMdxFiles } from '../constants/loaderMDX';
+import dynamic from 'next/dynamic';
 
 export default async function TeamCardsGallery() {
   const cardNames = await getMdxFiles('src/app/project/team/cards');
-  
+
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-center mb-8">Our Team</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {await Promise.all(cardNames.map(async (name) => {
-          const MDXModule = await importMdxComponent(name);
-          
-          return MDXModule ? (
-            <div 
-              key={name} 
-              className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-all hover:scale-105"
-            >
-              <MDXModule.default />
+    <div className="container mx-auto max-w-screen-lg py-4">
+      <h1 className="text-3xl font-bold text-center -mb-6">Our Team</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-2 justify-center">
+        {cardNames.map((name) => {
+          const MDXComponent = dynamic(() => import(`../app/project/team/cards/${name}.mdx`));
+
+          return (
+            <div key={name} className="w-full max-w-[300px] transform transition-all hover:scale-105 -mb-14">
+              <MDXComponent />
             </div>
-          ) : null;
-        }))}
+          );
+        })}
       </div>
     </div>
   );
