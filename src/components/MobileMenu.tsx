deeab8e6a415965559affa7838a6314f74ce45c3
Lazaro Alonso <lazarus.alon@gmail.com>
2025-03-05 17:01:43 +0100

    duplicated hover

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -25,7 +25,7 @@ const MobileMenu = ({ items }: MobileMenuProps) => {
           <div key={item.text} className="relative border-b border-[var(--toggle-bg)]">
             <div className="flex justify-between items-center px-4 py-2">
               <button
-                className="flex justify-between items-center w-full text-sm font-medium text-[var(--secondary-link-color)] hover:text-[var(--link-hover)] hover:bg-[var(--toggle-hover)] hover:rounded-lg mt-2 mx-2 px-4"
+                className="flex justify-between items-center w-full text-sm font-medium text-[var(--secondary-link-color)] hover:bg-[var(--toggle-hover)] hover:rounded-lg mt-2 mx-2 px-4"
                 onClick={() => {
                   if (item.link) {
                     window.location.href = item.link;
@@ -47,7 +47,7 @@ const MobileMenu = ({ items }: MobileMenuProps) => {
                   <Link
                     key={subItem.text}
                     href={subItem.link || '#'}
-                    className="block px-4 py-2 text-sm text-[var(--secondary-link-color)] hover:text-[var(--link-hover)] hover:bg-[var(--navbar-border)]"
+                    className="block px-4 py-2 text-sm text-[var(--secondary-link-color)] hover:bg-[var(--navbar-border)]"
                     role="menuitem"
                   >
                     {subItem.text}
