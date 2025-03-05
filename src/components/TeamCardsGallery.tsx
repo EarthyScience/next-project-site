e6f4fbe767277de8ac5b1b9a0cbae04be9044e06
Lazaro Alonso <lazarus.alon@gmail.com>
2025-03-05 16:45:00 +0100

    title team cards

--- src/components/TeamCardsGallery.tsx
+++ src/components/TeamCardsGallery.tsx
@@ -3,16 +3,18 @@ import dynamic from 'next/dynamic';
 
 interface TeamCardsGalleryProps {
   path?: string;
+  title?: string;
 }
 
 export default async function TeamCardsGallery({ 
-  path: cardPath = 'project/team/cards' 
+  path: cardPath = 'project/team/cards',
+  title = "Our Team"
 }: TeamCardsGalleryProps) {
   const cardNames = await getMdxFiles(`src/app/${cardPath}`);
 
   return (
     <div className="container mx-auto max-w-screen-lg py-4">
-      <h1 className="text-3xl font-bold text-center -mb-6">Our Team</h1>
+      <h1 className="text-3xl font-bold text-center -mb-6">{title}</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-2 justify-center">
         {cardNames.map((name) => {
           const MDXComponent = dynamic(() => import(`../app/${cardPath}/${name}.mdx`));
