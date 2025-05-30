c8b4a37bd4c39650f7c5dd3e944eadca5e9c69b5
Lazaro Alonso <lazarus.alon@gmail.com>
2025-05-30 11:30:53 +0200

    more options

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
+// import LogoProject from './Logo'
+import { FaBluesky } from "react-icons/fa6";
+
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -16,6 +19,8 @@ type MenuItem = {
   link?: string
   description?: string
   items?: MenuItem[]
+  isHome?: boolean
+  name?: string
 }
 
 type NavigationProps = {
@@ -32,14 +37,14 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            "block select-none rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
           )}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}</div>
+          <div className="text-sm font-medium mb-1">{title}</div>
           {children && (
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            <p className="text-sm leading-snug text-muted-foreground m-0">
               {children}
             </p>
           )}
@@ -60,17 +65,53 @@ const DesktopMenu = ({ items }: NavigationProps) => {
               <>
                 <NavigationMenuTrigger>{item.text}</NavigationMenuTrigger>
                 <NavigationMenuContent>
-                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                    {item.items.map((subItem) => (
-                      <ListItem
-                        key={subItem.text}
-                        title={subItem.text}
-                        href={subItem.link || '#'}
-                      >
+                  {item.isHome ? (
+                    <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                      <li className="row-span-3">
+                        <NavigationMenuLink asChild>
+                          <Link
+                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                            href={item.link || '/'}
+                          >
+                            <div className="mb-4 text-lg">
+                              <FaBluesky className="h-6 w-6" />
+                            </div>
+                            <div className="mb-2 text-lg font-medium">
+                              {item.name}
+                            </div>
+                            {item.description && (
+                              <p className="text-sm leading-tight text-muted-foreground">
+                              {item.description}
+                              </p>
+                            )}
+                          </Link>
+                        </NavigationMenuLink>
+                      </li>
+                      <ul className="col-span-1 space-y-2">
+                        {item.items.map((subItem) => (
+                          <ListItem
+                            key={subItem.text}
+                            title={subItem.text}
+                            href={subItem.link || '#'}
+                          >
+                            {subItem.description}
+                          </ListItem>
+                        ))}
+                      </ul>
+                    </ul>
+                  ) : (
+                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                      {item.items.map((subItem) => (
+                        <ListItem
+                          key={subItem.text}
+                          title={subItem.text}
+                          href={subItem.link || '#'}
+                        >
                         {subItem.description}
-                      </ListItem>
-                    ))}
-                  </ul>
+                        </ListItem>
+                      ))}
+                    </ul>
+                  )}
                 </NavigationMenuContent>
               </>
             ) : (
